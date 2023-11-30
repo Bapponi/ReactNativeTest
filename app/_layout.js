08b570e1a693ee6cf7366124a94faaef441bc6aa
Bapponi <aleksandarbubalo99@gmail.com>
2023-11-30 01:29:31 +0100

    stigao sam do 1:09:00 tutorijala

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
-import { Stack } from 'expo-router'
+import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-const layout = () => {
-    return <Stack />;
+SplashScreen.preventAutoHideAsync(); //da se vidid splash screen prilikom ucitavanja
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if(fontsLoaded){
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]) //useEffect, ili makar slicno
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />
 }
 
-export default layout;
+export default Layout;
